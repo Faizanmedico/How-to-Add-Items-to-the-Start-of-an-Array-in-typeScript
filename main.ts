c21a6df95ab10b9c3e11a1750a9b799686def593
Sultan Ahmed <146020547+Faizanmedico@users.noreply.github.com>
2024-04-27 13:14:58 +0500

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,8 @@
+const fruitsArray = ["Mango", "Apple"];
+
+fruitsArray.unshift("Orange", "Lemon", "Grape");
+
+console.log(fruitsArray);
+
+// The fruitsArray's invocation above will return:
+// ["Orange", "Lemon", "Grape", "Mango", "Apple"]
